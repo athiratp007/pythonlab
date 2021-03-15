c69c50dbf32c1f84368efa07fa5a7a673ce91ab1
athiratp007 <79839904+athiratp007@users.noreply.github.com>
2021-03-15 15:25:58 +0530

    Add files via upload

---
+++ color list.py
@@ -0,0 +1,5 @@
+color1=input("enter a set of color(space seperated):")
+color2=input("enter a set of color(space seperated):")
+c1=set(color1.split())
+c2=set(color2.split())
+print("the different between",c1,"and",c2,"is",c1.difference(c2))
---
+++ dict.py
@@ -0,0 +1,6 @@
+d={1:2,3:4,4:3,2:1,0:0}
+print('original dictionary:',d)
+sorted_d=sorted(d.items(),key=operator.itemgetter(0))
+print('dictonary in ascending order by value:',sorted_d)
+sorted_d=sorted(d.items(),key=operator.itemgetter(0),reverse=true)
+print('dictionary in descending order by value:',sorted_d)
